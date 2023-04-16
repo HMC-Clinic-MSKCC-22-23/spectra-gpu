e3871b1668084dda856670068a1ad6bbf56c34af
muffinkate <theawesomekate@gmail.com>
2023-04-15 20:35:36 -0700

    working on lightning module

--- spectra/SpectraWithLightning.py
+++ spectra/SpectraWithLightning.py
@@ -20,13 +20,31 @@ from torch.distributions.log_normal import LogNormal
 from torch.distributions.dirichlet import Dirichlet
 
 class SPECTRA(nn.Module):
-   return 0
+   def __init__(self):
+       return 0
            
 class SPECTRA_DataModule(pl.LightningDataModule):
-    return 0
+    def __init__(self):
+        return 0
 
 class SPECTRA_LitModel(pl.LightningModule):
-    return 0
+    def __init__(self, internal_model):
+        super().__init__()
+        self.internal_model = internal_model
+
+    def training_step(self, batch, batch_idx):
+        # training_step defines the train loop.
+        x, y = batch
+        x = x.view(x.size(0), -1)
+        z = self.encoder(x)
+        x_hat = self.decoder(z)
+        loss = F.mse_loss(x_hat, x)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
 
 class est_spectra():
-    return 0
+    def __init__(self):
+        return 0
